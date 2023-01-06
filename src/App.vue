0886f350793a224fd0cd33954da7738fed4f82be
Azis Ramadhan <git@azram.eu.org>
2023-01-06 14:34:36 +0700

    Initial Implementation

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+let url = window.location.host
+let ownerName = "Azis Ramadhan"
+</script>
+
+<template>
+  <main>
+    <h1>Hi there!</h1>
+    <h2><span>{{ url }}</span> is owned by {{ ownerName }}</h2>
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  height: 100vh;
+  height: 100svh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+span {
+  color: coral;
+}
+</style>
